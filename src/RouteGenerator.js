adae22366884efc6fd820c34fb5fcba11304ca79
Gerald Monaco <gerald@gmonaco.me>
2015-08-19 21:05:40 -0700

    Replace Immutable with WeakMap

--- src/RouteGenerator.js
+++ src/RouteGenerator.js
@@ -2,15 +2,15 @@ import * as Immutable from 'immutable';
 
 export default class RouteGenerator {
   constructor() {
-    this._routeToIndexMap = new Immutable.Map();
+    this._routeToIndexMap = new WeakMap();
   }
 
   getRouteFor(branch) {
     return '$$_' + branch.map(route => {
       let id = this._routeToIndexMap.get(route);
       if (!id) {
-        id = this._routeToIndexMap.size;
-        this._routeToIndexMap = this._routeToIndexMap.set(route, id);
+        id = this._routeToIndexMap.length;
+        this._routeToIndexMap.set(route, id);
       }
       return id;
     }).join('.');
