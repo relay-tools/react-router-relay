0a6f7b3ad0b8358eaef72a42414416b2c7b7da9b
Jimmy Jia <tesrin@gmail.com>
2015-08-27 17:02:58 -0400

    Refactor and update API
    * Use createElement instead of root route
    * Support per-route render callbacks

---
+++ src/Container.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import StaticContainer from 'react-static-container';
+
+import getParamsForRoute from './getParamsForRoute';
+import RootComponent from './RootComponent';
+import RouteAggregator from './RouteAggregator';
+
+export default class Container extends React.Component {
+  static displayName = 'ReactRouterRelay.Container';
+
+  static propTypes = {
+    Component: React.PropTypes.func.isRequired,
+    routerProps: React.PropTypes.object.isRequired,
+  };
+
+  static contextTypes = {
+    routeAggregator: React.PropTypes.instanceOf(RouteAggregator),
+  };
+
+  render() {
+    const {routeAggregator} = this.context;
+    if (!routeAggregator) {
+      return <RootComponent {...this.props} />;
+    }
+
+    const {Component, routerProps} = this.props;
+    const {route} = routerProps;
+
+    // FIXME: Remove once fix for facebook/react#4218 is released.
+    const {children} = routerProps;
+    if (children) {
+      routerProps.children = React.cloneElement(children, {});
+    }
+
+    const {queries} = route;
+    if (!queries) {
+      return <Component {...routerProps} />;
+    }
+
+    const params = getParamsForRoute(routerProps);
+    const {fragmentPointers, failure} =
+      routeAggregator.getData(queries, params);
+
+    let shouldUpdate = true;
+    let element;
+
+    // This is largely copied from RelayRootContainer#render.
+    if (failure) {
+      const {renderFailure} = route;
+      if (renderFailure) {
+        const [error, retry] = failure;
+        element = renderFailure(error, retry);
+      } else {
+        element = null;
+      }
+    } else if (fragmentPointers) {
+      const data = {...routerProps, ...params, ...fragmentPointers};
+
+      const {renderFetched} = route;
+      if (renderFetched) {
+        element = renderFetched(data);
+      } else {
+        element = <Component {...data} />;
+      }
+    } else {
+      const {renderLoading} = route;
+      if (renderLoading) {
+        element = renderLoading();
+      } else {
+        element = undefined;
+      }
+
+      if (element === undefined) {
+        element = null;
+        shouldUpdate = false;
+      }
+    }
+
+    return (
+      <StaticContainer shouldUpdate={shouldUpdate}>
+        {element}
+      </StaticContainer>
+    );
+  }
+}
