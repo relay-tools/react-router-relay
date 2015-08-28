10be8f93f08107cf0f18852076a713eabba8a2b3
Samuel Gwilym <sam@comique.co>
2015-08-28 16:58:01 +0200

    Pass all props to children through Container

--- src/Container.js
+++ src/Container.js
@@ -23,7 +23,7 @@ export default class Container extends React.Component {
       return <RootComponent {...this.props} />;
     }
 
-    const {Component, routerProps} = this.props;
+    const {Component, ...routerProps} = this.props;
     const {route} = routerProps;
 
     // FIXME: Remove once fix for facebook/react#4218 is released.
